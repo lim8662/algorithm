580e8234d349ede883c12cb771136da7feb0ef3a
lim8662 <lim0988@naver.com>
2021-01-22 00:14:12 +0900

    solved

--- src/boj_9375.java
+++ src/boj_9375.java
@@ -13,30 +13,33 @@ public class boj_9375 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Map<String, Integer> map = new HashMap<>();
+		Map<String, Integer> map;
 		StringTokenizer st;
 		
 		
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 0; tc < T; tc++) {
 			int N = Integer.parseInt(br.readLine());
+			map = new HashMap<>();
 			
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				st.nextToken();
 				String type = st.nextToken();
-				
+
 				if(map.containsKey(type)) {
 					int cnt = map.get(type);
-					map.put(type, cnt++);
+					map.put(type, cnt + 1);
 				} else {
 					map.put(type, 1);
 				}
 			}
-			
+			int count = 1;
 			for(String type : map.keySet()) {
-				
+				count *= map.get(type) + 1; // 착용하지 않는 경우의 수도 포함
 			}
+			
+			System.out.println(count - 1); // 알몸인 경우 제외
 		}
 		
 	}
