ccb65466272d3b863262fb39ea1c8614194082b1
lim8662 <lim0988@naver.com>
2021-04-09 23:56:18 +0900

    04/09 solved

---
+++ src/BOJ/boj_17070_pipeMove1.java
@@ -0,0 +1,47 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_17070_pipeMove1 {
+	
+	static int R, C, T, pos;
+	static int[][] map, tmap;
+	static final int[] dx = {0, 0, 1, -1};
+	static final int[] dy = {1, -1, 0 , 0};
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken()); 
+		C = Integer.parseInt(st.nextToken()); 
+		T = Integer.parseInt(st.nextToken());
+		map = new int[R][C];
+		
+		for (int i = 0; i < R; i++) { // 맵 입력
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j] == -1) pos = i;
+			}
+		}
+		
+		while(T > 0) { // T초만큼 시뮬레이션	
+			
+			T--;
+		}
+		
+		
+		int total = 0; // 남은 미세먼지 총량
+		for (int i = 0; i < R; i++) {
+			for (int j = 0; j < C; j++) {
+				if( map[i][j] > 0 ) total += map[i][j];
+			}
+		}
+		System.out.println(total);
+	}
+
+	
+}
