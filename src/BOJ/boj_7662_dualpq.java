4ce7adb9dd85ae5acdd806121910ecc2ee1bcfff
lim8662 <lim0988@naver.com>
2021-05-04 23:12:54 +0900

    05/04 solved

---
+++ src/BOJ/boj_7662_dualpq.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_7662_dualpq {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(br.readLine());
+		int[] arr = null;
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			arr = new int[N];
+			for (int i = 0; i < N; i++) {
+				
+			}
+		}
+		
+		int r = Integer.parseInt(st.nextToken());
+	
+		
+	}
+	
+	
+}
