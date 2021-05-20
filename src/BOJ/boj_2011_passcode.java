381b4dc38f000b1b7edf99734db83806cc51f831
lim8662 <lim0988@naver.com>
2021-05-20 23:48:27 +0900

    .

---
+++ src/BOJ/boj_2011_passcode.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_2011_passcode {
+	static int N, r ,c;
+	static final int[][] order = {{0,0}, {0,1}, {1,0}, {1,1}}; // Z 방문 순서
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+		//StringTokenizer st = new StringTokenizer(br.readLine());
+		char c = 'z';
+		System.out.println('a' + 26);
+	}
+	
+}
