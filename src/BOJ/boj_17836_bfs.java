0290f5d9e81e50c5db329c8b50460f2541163850
lim8662 <lim0988@naver.com>
2021-06-05 00:02:36 +0900

    .

--- src/BOJ/boj_17836_bfs.java
+++ src/BOJ/boj_17836_bfs.java
@@ -7,13 +7,28 @@ import java.util.StringTokenizer;
 
 public class boj_17836_bfs {
 	static int N, M ,T;
+	static int[][] map;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		T = Integer.parseInt(st.nextToken());
-
+		map = new int[M][N];
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.printf("%d ", map[i][j]);
+			}
+			System.out.println();
+		}
 	}
 	
 	
