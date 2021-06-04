e11de00a8b4116fcc24a7aadfd3fc498202e89e7
lim8662 <lim0988@naver.com>
2021-06-04 23:59:20 +0900

    .

--- src/BOJ/boj_17836_bfs.java
+++ src/BOJ/boj_17836_bfs.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class boj_17836_bfs {
-	static int N, r ,c;
+	static int N, M ,T;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(st.nextToken());
 
 	}
 	
