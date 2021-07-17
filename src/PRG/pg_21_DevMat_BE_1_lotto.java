2f8d38cfe27d97e882a15bfcc967d53bf53554c3
임영찬 <lim8662@gmail.com>
2021-07-17 17:47:56 +0900

    .

--- src/PRG/pg_21_DevMat_BE_1_lotto.java
+++ src/PRG/pg_21_DevMat_BE_1_lotto.java
@@ -1,6 +1,8 @@
 package PRG;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class pg_21_DevMat_BE_1_lotto {
 
@@ -10,26 +12,23 @@ public class pg_21_DevMat_BE_1_lotto {
 		System.out.println(Arrays.toString(new pg_21_DevMat_BE_1_lotto().solution(lottos, win_nums)));
 	}
 	
-	static int[] rank = {6, 6, 5, 4, 3, 2, 1}; // �´� ���ں� ����
+	static int[] rank = {6, 6, 5, 4, 3, 2, 1}; // 맞는 숫자별 순위
 	
 	public int[] solution(int[] lottos, int[] win_nums) {
+		int blank = 0; // 빈 칸의 수
+		int match = 0; // 맞춘 번호의 수
+		Map<Integer, Boolean> map = new HashMap<>();
 		
-		Arrays.sort(lottos);
-		
-		int blank = 0; // �� ĭ�� ��
-		int match = 0; // ���� ��ȣ�� ��
-		for (int i = 0; i < lottos.length; i++) {
-			int cur = lottos[i];
-			if(cur == 0) {
+		for (int lotto : lottos) {
+			if(lotto == 0) {
 				blank++; continue;
 			}
-			for (int j = 0; j < win_nums.length; j++) {
-				if(cur == win_nums[j]) {
-					match++; break;
-				}
-			}
+			map.put(lotto, true);
+		}
+		
+		for (int win : win_nums) {
+			if(map.containsKey(win)) match++;
 		}
 		return new int[] {rank[match + blank], rank[match]};
 	}
-
 }
