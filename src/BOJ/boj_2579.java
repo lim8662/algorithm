9e7e7c32ba084a05381f43eabe990425aaa8371d
lim8662 <lim8662@gmail.com>
2021-08-06 23:59:26 +0900

    .

--- src/BOJ/boj_2579.java
+++ src/BOJ/boj_2579.java
@@ -4,18 +4,20 @@ import java.io.*;
 import java.util.*;
 
 public class boj_2579 {
-	// 계단 오르기
-	static Integer[][] dp = new Integer[41][2];
+	// 계단 오르기 
 	// 얻을 수 있는 총 점수의 최댓값 리턴
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 계단의 수(1~300개)
 		int ans = 0;
-		int[] stairs = new int[N];
-		for (int i = 0; i < N; i++) {
+		Integer[][] dp = new Integer[N][2];
+		int[] stairs = new int[N+1];
+		for (int i = 1; i <= N; i++) {
 			stairs[i] = Integer.parseInt(br.readLine()); // 계단 점수 입력(1~1만)
 		}
 		// 1~2칸 이동 가능, 3연속 1칸은 불가능, 마지막 계단에 도착
+		
+		
 		
 		
 		System.out.println(ans);
