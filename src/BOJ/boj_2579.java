b865a353605caa7bb181a2efda1fab69b5fc7667
lim8662 <lim8662@gmail.com>
2021-08-07 23:46:03 +0900

    .

--- src/BOJ/boj_2579.java
+++ src/BOJ/boj_2579.java
@@ -10,17 +10,22 @@ public class boj_2579 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()); // 계단의 수(1~300개)
 		int ans = 0;
-		Integer[][] dp = new Integer[N][2];
+		int[] dp = new int[N+1]; // 각 계단에 도달하여 얻을 수 있는 최대 총 점수
 		int[] stairs = new int[N+1];
 		for (int i = 1; i <= N; i++) {
 			stairs[i] = Integer.parseInt(br.readLine()); // 계단 점수 입력(1~1만)
 		}
 		// 1~2칸 이동 가능, 3연속 1칸은 불가능, 마지막 계단에 도착
+		dp[1] = stairs[1];
+		if(N >= 2) dp[2] = stairs[2] + dp[1];
+		
+		for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i-2] , dp[i-3] + stairs[i-1]) + stairs[i];
+		}
 		
 		
 		
-		
-		System.out.println(ans);
+		System.out.println(dp[N]);
 	}
 	
 	
