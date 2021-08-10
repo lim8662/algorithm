959c4f3db4078e9e1950898bda8d176c356975d2
lim8662 <lim8662@gmail.com>
2021-08-10 23:57:27 +0900

    .

---
+++ src/BOJ/boj_2606_virus.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class boj_2606_virus {
+	// 서로 연결된 컴퓨터 정보
+	// 1번에서 시작해 바이러스에 걸리게 되는 컴퓨터의 수 출력
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 컴퓨터 수(100이하)
+		int P = Integer.parseInt(br.readLine()); // 컴퓨터 쌍의 수
+		
+		
+	}
+	
+	
+}
